62c50b9caf5f872beb99fd84f553c1414027a636
Joao Carlos <carlos.rocha.assis@gmail.com>
2021-04-10 22:12:05 -0300

    Header Create

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Ínicio | ig.News</title>
+    </Head>
+      <h1>
+        Novo projeto em Next
+      </h1>
+    </>
+  )
+}
